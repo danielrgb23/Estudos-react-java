10b7e037bb4d6f668139386b87653f40e290ef26
Daniel <danielrgb2@gmail.com>
2022-09-05 16:42:03 -0300

    Feat:creação do projeto

---
+++ backend/src/main/java/com/danielrgb23/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.danielrgb23.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/danielrgb23/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielrgb23.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
