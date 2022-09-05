2be290a0bd04393a2bce9330d7b1519ee2a9033f
Daniel <danielrgb2@gmail.com>
2022-09-05 17:26:33 -0300

    Feat:botao de notificação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { NotificationButton } from "./components/notificationButton";
+
 export function App() {
   return (
     <div className="App">
         <h1>hello world</h1>
+        <NotificationButton />
     </div>
   )
 }
