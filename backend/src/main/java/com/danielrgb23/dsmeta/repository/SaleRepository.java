676671f7eb7e672f387158e906880cb41c385833
Daniel <danielrgb2@gmail.com>
2022-09-07 14:25:19 -0300

    Feat:teste da Api

---
+++ backend/src/main/java/com/danielrgb23/dsmeta/repository/SaleRepository.java
@@ -0,0 +1,8 @@
+package com.danielrgb23.dsmeta.repository;
+
+import com.danielrgb23.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/danielrgb23/dsmeta/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.danielrgb23.dsmeta.services;
+
+import com.danielrgb23.dsmeta.entities.Sale;
+import com.danielrgb23.dsmeta.repository.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public List<Sale> findSales(){
+        return repository.findAll();
+    }
+}
