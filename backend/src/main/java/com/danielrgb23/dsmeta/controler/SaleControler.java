9801f869d4ffe663eb3d91518bd0ea6d3620c217
Daniel <danielrgb2@gmail.com>
2022-09-07 16:31:35 -0300

    Feat:consulta por data

--- backend/src/main/java/com/danielrgb23/dsmeta/controler/SaleControler.java
+++ backend/src/main/java/com/danielrgb23/dsmeta/controler/SaleControler.java
@@ -3,11 +3,14 @@ package com.danielrgb23.dsmeta.controler;
 import com.danielrgb23.dsmeta.entities.Sale;
 import com.danielrgb23.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,7 +20,11 @@ public class SaleControler {
     private SaleService service;
 
     @GetMapping
-    public List<Sale> findSales(){
-        return service.findSales();
+    public Page<Sale> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable){
+
+        return service.findSales(minDate, maxDate,pageable);
     }
 }
