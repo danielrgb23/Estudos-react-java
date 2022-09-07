a643e93694dbfa31c41d8bb4e62f6b8a7875c803
Daniel <danielrgb2@gmail.com>
2022-09-07 16:59:34 -0300

    Feat:adicionando sms

--- backend/src/main/java/com/danielrgb23/dsmeta/controler/SaleControler.java
+++ backend/src/main/java/com/danielrgb23/dsmeta/controler/SaleControler.java
@@ -2,14 +2,12 @@ package com.danielrgb23.dsmeta.controler;
 
 import com.danielrgb23.dsmeta.entities.Sale;
 import com.danielrgb23.dsmeta.services.SaleService;
+import com.danielrgb23.dsmeta.services.SmsService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,6 +17,9 @@ public class SaleControler {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
@@ -26,5 +27,9 @@ public class SaleControler {
             Pageable pageable){
 
         return service.findSales(minDate, maxDate,pageable);
+    }
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
     }
 }
