c5b25f7c5f82235b59437d55456e8b41c418399c
Daniel <danielrgb2@gmail.com>
2022-09-17 12:21:38 -0300

    Feat:noticação de envio de sms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
-import { NotificationButton } from "./components/notificationButton";
 import { SalesCard } from "./components/SalesCard";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
